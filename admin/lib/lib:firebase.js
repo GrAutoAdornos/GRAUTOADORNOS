ec769ebe9dca16da812e8e9824d08481689997d6
GrAutoAdornos <grautoadorno@gmail.com>
2026-09-25 17:30:27 -0400

    Add files via upload

---
+++ admin/lib/lib:firebase.js
@@ -0,0 +1,20 @@
+// lib/firebase.js
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDJltPlA8kjDzWnYFNkQnRBxFylSH52Tg",
+  authDomain: "gr-auto-adornos.firebaseapp.com",
+  projectId: "gr-auto-adornos",
+  storageBucket: "gr-auto-adornos.firebasestorage.app",
+  messagingSenderId: "387692589652",
+  appId: "1:387692589652:web:25cdeb4fcb183e00f133c1",
+  measurementId: "G-XD4DMESRJ4"
+};
+
+// Evitar la inicialización duplicada en Next.js
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export default app;
