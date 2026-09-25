a9a735cd7e0b6fe5a7ac8bf704df8e0f127f4062
GrAutoAdornos <grautoadorno@gmail.com>
2026-09-25 18:31:29 -0400

    Add Head component for favicon and apple touch icon

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
-// pages/_app.js
-import '../styles/globals.css';
+import Head from 'next/head';
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Head>
+        <link rel="icon" href="/LOGO NEGRO.jpeg" type="image/png" />
+        <link rel="apple-touch-icon" href="/LOGO NEGRO.jpeg" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
