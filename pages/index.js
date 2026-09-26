33eef9e8131a042442cab2650d7e63bf740dea90
GrAutoAdornos <grautoadorno@gmail.com>
2026-09-25 21:53:46 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -750,7 +750,7 @@ export default function Home() {
       <footer style={{ backgroundColor: '#000000', borderTop: '1px solid #222', padding: '25px', textAlign: 'center', fontSize: '12px', color: '#666' }}>
         <p style={{ margin: '0 0 5px' }}> Correo: <a href="mailto:grautoadornos@gmail.com" style={{ color: '#AAA', textDecoration: 'none' }}>grautoadornos@gmail.com</a> | WhatsApp: 849-404-0514</p>
         <p style={{ margin: 0 }}>© 2026 GR Auto Adornos. Todos los derechos reservados.</p>
-      </footer>
+     </footer>
     </div>
   );
 }
